64dd81079bbdac39a86d76f3c185f52774955be2
Michael Campbell <michaelmichael323@hotmail.com>
2022-04-14 20:13:36 -0600

    Finished no extra credit

--- password.js
+++ password.js
@@ -9,3 +9,22 @@
 // If the user’s password meets the constraint, show a success message to the user
 
 // If the user’s password fails the constraint, show a failure message to the user
+
+console.log('Welcome to the password validator tool')
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+reader.question("What is your password?", function(input){
+		
+    if(input.length >= 10){
+		console.log('success')
+	} else {
+    console.log('failure')
+  }
+	reader.close()
+
+});
